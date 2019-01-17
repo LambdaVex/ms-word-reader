43252fb4250305c63890610c93366312af0a1744
Ali Denno <aliedenno@gmail.com>
2019-01-17 15:36:44 +0100

    Initial Push

---
+++ src/main/java/reader/Wordreader.java
@@ -0,0 +1,17 @@
+package reader;
+
+import java.io.IOException;
+
+class element{
+    String style;
+    String value;
+    public element(String style, String value) {
+        this.style = style;
+        this.value = value;
+    }
+}
+public class Wordreader {
+    public static void main(String[] args) {
+
+    }
+}
